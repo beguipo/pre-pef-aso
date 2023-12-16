cdcee27050e5995497506a6c956d5524f5685b7a
Jose <jose.berriales@edu.uah.es>
2023-12-16 15:30:28 +0000

    Version 2

--- Dockerfile
+++ Dockerfile
@@ -6,4 +6,4 @@ COPY ./app /app
 
 RUN pip install -r requirements.txt
 
-CMD ["python3","main.py"]
+CMD ["python3", "main.py"]
--- app/main.py
+++ app/main.py
@@ -1,14 +1,28 @@
-from flask import Flask
+import flask
+import socket
 import os
-app = Flask(__name__)
 
-@app.route('/hello')
-def hello_world():
-    return ("Hola Mundo, soy Python!")
+num_visitas = 0
+# Crear el objeto que representa la aplicacion web
+APP = flask.Flask(__name__)
 
-@app.route('/bye')
-def bye_world():
-    return ("Adios mundo cruel")
+@APP.route('/')
+def index():
+    global num_visitas
+
+    """ Muestra la página inicial asociada al recurso `/`
+        y que estará contenida en el archivo index.html
+    """
+    userinfo = {
+        'surname': os.environ['CLIENT_NAME']
+    }
+    num_visitas = num_visitas + 1 
+    hostname = socket.gethostname()
+    return flask.render_template('index.html', vis=num_visitas,
+            user=userinfo, server_name=hostname)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=os.environ["PORT"])
+    PORT=os.environ['PORT']
+    APP.debug=True
+    APP.run(host='0.0.0.0', port=PORT)
+
